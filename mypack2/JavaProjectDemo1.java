16f491c5d4d73759518ea3a857646f639252a135
samba <rao.samba@gmail.com>
2020-11-29 21:39:44 +0530

    demo1

---
+++ mypack2/JavaProjectDemo1.java
@@ -0,0 +1,11 @@
+package mypack2;
+
+public class JavaProjectDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java project demo with Jenkins@@@@");
+
+	}
+
+}
